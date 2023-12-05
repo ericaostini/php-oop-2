636375830790d933bfd44a9c67b92b644a0c485c
ericaostini <ericaostini14@gmail.com>
2023-12-05 15:24:20 +0100

    Books class and function construct creation

--- books.php
+++ books.php
@@ -1,6 +1,26 @@
 <?php
 include __DIR__ . "/Views/header.php";
+
+class Books
+{
+    private $id;
+    private $title;
+    private $long_description;
+    private $thumbnail_url;
+    private $author;
+
+    public function __construct($_id, $title, $_description, $_url, $_author)
+    {
+        $this->id = $_id;
+        $this->title = $title;
+        $this->long_description = $_description;
+        $this->thumbnail_url = $_url;
+        $this->author = $_author;
+    }
+}
 ?>
+
+
 
 <?php
 include __DIR__ . "/Views/footer.php";
